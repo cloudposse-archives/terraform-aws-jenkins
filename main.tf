4ae9d649fb89886175b7788fd94c0f1dabcf452c
aknysh <aknysh>
2017-09-14 20:49:16 -0400

    Add Terraform modules

---
+++ main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_version = ">= 0.10.2"
+}
+
+# Elastic Beanstalk Application
+module "eb_application" {
+  source      = "git::https://github.com/cloudposse/tf_eb_application.git?ref=tags/0.1.1"
+  namespace   = "${var.namespace}"
+  name        = "${var.name}"
+  stage       = "${var.stage}"
+  description = "${var.description}"
+}
+
+# Elastic Beanstalk Environment
+module "eb_environment" {
+  source                  = "git::https://github.com/cloudposse/tf_eb_environment.git?ref=tags/0.2.0"
+  attributes              = ["eb"]
+  namespace               = "${var.namespace}"
+  name                    = "${var.name}"
+  stage                   = "${var.stage}"
+  zone_id                 = "${var.zone_id}"
+  app                     = "${module.eb_application.app_name}"
+  autoscale_lower_bound   = "${var.autoscale_lower_bound}"
+  autoscale_upper_bound   = "${var.autoscale_upper_bound}"
+  autoscale_min           = "${var.autoscale_min}"
+  autoscale_max           = "${var.autoscale_max}"
+  instance_type           = "${var.eb_instance_type}"
+  updating_min_in_service = "${var.updating_min_in_service}"
+  updating_max_batch      = "${var.updating_max_batch}"
+  healthcheck_url         = "${var.healthcheck_url}"
+  loadbalancer_type       = "${var.loadbalancer_type}"
+  vpc_id                  = "${var.vpc_id}"
+  public_subnets          = "${var.public_subnets}"
+  private_subnets         = "${var.private_subnets}"
+  security_groups         = ["${var.security_groups}"]
+  keypair                 = "${var.keypair}"
+  solution_stack_name     = "${var.solution_stack_name}"
+}
+
+# Elastic Container Registry Docker Repository
+module "ecr" {
+  source     = "git::https://github.com/cloudposse/tf_ecr.git?ref=tags/0.2.0"
+  attributes = ["ecr"]
+  namespace  = "${var.namespace}"
+  name       = "${var.name}"
+  stage      = "${var.stage}"
+}
+
+# CodePipeline/CodeBuild
+module "cicd" {
+  source             = "git::https://github.com/cloudposse/tf_cicd.git?ref=tags/0.4.0"
+  attributes         = ["cicd"]
+  namespace          = "${var.namespace}"
+  name               = "${var.name}"
+  stage              = "${var.stage}"
+  app                = "${module.eb_application.app_name}"
+  env                = "${module.eb_environment.name}"
+  enabled            = true
+  github_oauth_token = "${var.github_oauth_token}"
+  repo_owner         = "${var.github_organization}"
+  repo_name          = "${var.github_repo_name}"
+  branch             = "${var.github_branch}"
+  build_image        = "${var.build_image}"
+  build_compute_type = "${var.build_compute_type}"
+  privileged_mode    = true
+  aws_region         = "${var.region}"
+  aws_account_id     = "${var.aws_account_id}"
+  image_repo_name    = "${var.image_repo_name}"
+  image_tag          = "${var.image_tag}"
+}
---
+++ variables.tf
@@ -0,0 +1,136 @@
+variable "region" {
+  type    = "string"
+  default = "us-west-2"
+}
+
+variable "namespace" {
+  type = "string"
+}
+
+variable "name" {
+  type = "string"
+}
+
+variable "stage" {
+  type = "string"
+}
+
+variable "description" {
+  type    = "string"
+  default = ""
+}
+
+variable "solution_stack_name" {
+  type    = "string"
+  default = "64bit Amazon Linux 2017.03 v2.7.3 running Docker 17.03.1-ce"
+}
+
+variable "vpc_id" {
+  type = "string"
+}
+
+variable "public_subnets" {
+  type = "list"
+}
+
+variable "private_subnets" {
+  type = "list"
+}
+
+variable "zone_id" {
+  type = "string"
+}
+
+variable "updating_min_in_service" {
+  default = "1"
+}
+
+variable "updating_max_batch" {
+  default = "1"
+}
+
+variable "autoscale_lower_bound" {
+  default = "20"
+}
+
+variable "autoscale_upper_bound" {
+  default = "80"
+}
+
+variable "healthcheck_url" {
+  type    = "string"
+  default = "/"
+}
+
+variable "loadbalancer_type" {
+  type    = "string"
+  default = "application"
+}
+
+variable "eb_instance_type" {
+  type    = "string"
+  default = "t2.medium"
+}
+
+variable "autoscale_min" {
+  default = "1"
+}
+
+variable "autoscale_max" {
+  default = "1"
+}
+
+variable "security_groups" {
+  type    = "list"
+  default = []
+}
+
+variable "keypair" {
+  type    = "string"
+  default = ""
+}
+
+# CI/CD - CodePipeline/CodeBuild
+variable "github_oauth_token" {
+  type = "string"
+  default = ""
+}
+
+variable "github_organization" {
+  type = "string"
+}
+
+variable "github_repo_name" {
+  type = "string"
+}
+
+variable "github_branch" {
+  type = "string"
+}
+
+# http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html#build-env-ref-available
+variable "build_image" {
+  type    = "string"
+  default = "aws/codebuild/docker:1.12.1"
+}
+
+# http://docs.aws.amazon.com/codebuild/latest/userguide/build-env-ref.html#build-env-ref-compute-types
+variable "build_compute_type" {
+  type    = "string"
+  default = "BUILD_GENERAL1_SMALL"
+}
+
+variable "aws_account_id" {
+  type        = "string"
+  description = "AWS Account ID. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "image_repo_name" {
+  type        = "string"
+  description = "ECR repository name to store the Docker image built by this module. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "image_tag" {
+  type        = "string"
+  description = "Docker image tag in the ECR repository, e.g. 'latest'. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
