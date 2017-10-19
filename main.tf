7fdc0dd2a3d6a6dd22a6ce1c5e562771ed76ac82
aknysh <aknysh>
2017-10-18 22:54:33 -0400

    Add IAM Policy to the EC2 instance profile to allow Jenkins master to launch and control slave EC2 instances

--- main.tf
+++ main.tf
@@ -176,3 +176,49 @@ resource "aws_security_group" "slaves" {
 
   tags = "${module.label_slaves.tags}"
 }
+
+# Policy document with permissions to launch new EC2 instances
+# https://wiki.jenkins.io/display/JENKINS/Amazon+EC2+Plugin
+data "aws_iam_policy_document" "slaves" {
+  statement {
+    sid = "AllowLaunchingEC2Instances"
+
+    actions = [
+      "ec2:DescribeSpotInstanceRequests",
+      "ec2:CancelSpotInstanceRequests",
+      "ec2:GetConsoleOutput",
+      "ec2:RequestSpotInstances",
+      "ec2:RunInstances",
+      "ec2:StartInstances",
+      "ec2:StopInstances",
+      "ec2:TerminateInstances",
+      "ec2:CreateTags",
+      "ec2:DeleteTags",
+      "ec2:DescribeInstances",
+      "ec2:DescribeKeyPairs",
+      "ec2:DescribeRegions",
+      "ec2:DescribeImages",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "iam:PassRole",
+    ]
+
+    resources = ["*"]
+    effect    = "Allow"
+  }
+}
+
+# Policy for the EB EC2 instance profile to allow launching Jenkins slaves
+resource "aws_iam_policy" "slaves" {
+  name        = "${module.label_slaves.id}"
+  path        = "/"
+  description = "Policy for EC2 instance profile to allow launching Jenkins slaves"
+  policy      = "${data.aws_iam_policy_document.slaves.json}"
+}
+
+# Attach Policy to the EC2 instance profile to allow Jenkins master to launch and control slave EC2 instances
+resource "aws_iam_role_policy_attachment" "slaves" {
+  role       = "${module.elastic_beanstalk_environment.ec2_instance_profile_role_name}"
+  policy_arn = "${aws_iam_policy.slaves.arn}"
+}
