52f3a211649fcee4e362b186775d6b7b3c528d1b
aknysh <aknysh>
2018-01-05 17:29:53 -0500

    Add `existing_vpc_new_subnets` example

--- examples/existing_vpc_new_subnets/main.tf
+++ examples/existing_vpc_new_subnets/main.tf
@@ -16,7 +16,7 @@ module "jenkins" {
   aws_region                   = "us-west-2"
   availability_zones           = ["${data.aws_availability_zones.available.names}"]
   solution_stack_name          = "64bit Amazon Linux 2017.03 v2.7.4 running Docker 17.03.2-ce"
-  vpc_id                       = "${module.vpc.vpc_id}"
+  vpc_id                       = "vpc-a22222ee"
   zone_id                      = "ZXXXXXXXXXXX"
   public_subnets               = "${module.subnets.public_subnet_ids}"
   private_subnets              = "${module.subnets.private_subnet_ids}"
@@ -55,21 +55,6 @@ module "jenkins" {
   }
 }
 
-module "vpc" {
-  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
-  namespace  = "cp"
-  name       = "jenkins"
-  stage      = "prod"
-  cidr_block = "10.0.0.0/16"
-  delimiter  = "-"
-  attributes = ["vpc"]
-
-  tags = {
-    BusinessUnit = "ABC"
-    Department   = "XYZ"
-  }
-}
-
 module "subnets" {
   source                     = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
   availability_zones         = ["${data.aws_availability_zones.available.names}"]
@@ -77,13 +62,13 @@ module "subnets" {
   name                       = "jenkins"
   stage                      = "prod"
   region                     = "us-west-2"
-  vpc_id                     = "${module.vpc.vpc_id}"
-  igw_id                     = "${module.vpc.igw_id}"
+  vpc_id                     = "vpc-a22222ee"
+  igw_id                     = "igw-s32321vd"
   cidr_block                 = "10.0.0.0/16"
   nat_gateway_enabled        = "true"
-  vpc_default_route_table_id = "${module.vpc.vpc_default_route_table_id}"
-  public_network_acl_id      = "${module.vpc.vpc_default_network_acl_id}"
-  private_network_acl_id     = "${module.vpc.vpc_default_network_acl_id}"
+  vpc_default_route_table_id = "ZXXXXXXXXXXX"
+  public_network_acl_id      = "ZXXXXXXXXXXX"
+  private_network_acl_id     = "ZXXXXXXXXXXX"
   delimiter                  = "-"
   attributes                 = ["subnet"]
 
