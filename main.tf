b6362fa68ed7c44aa84ac1eb7c0de5f08fe0160a
aknysh <aknysh>
2018-01-07 22:03:33 -0500

    Bump `terraform-aws-elastic-beanstalk-environment` version to `0.3.3`

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "elastic_beanstalk_application" {
 
 # Elastic Beanstalk Environment
 module "elastic_beanstalk_environment" {
-  source        = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=tags/0.3.2"
+  source        = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=tags/0.3.3"
   namespace     = "${var.namespace}"
   name          = "${var.name}"
   stage         = "${var.stage}"
