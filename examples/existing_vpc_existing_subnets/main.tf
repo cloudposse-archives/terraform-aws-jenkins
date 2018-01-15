de68a4e044df82e6ac0cfd71644dd58134843ff2
aknysh <aknysh>
2018-01-15 13:43:13 -0500

    Update examples

--- examples/existing_vpc_existing_subnets/main.tf
+++ examples/existing_vpc_existing_subnets/main.tf
@@ -15,12 +15,10 @@ module "jenkins" {
   aws_account_id               = "000111222333"
   aws_region                   = "us-west-2"
   availability_zones           = ["${data.aws_availability_zones.available.names}"]
-  solution_stack_name          = "64bit Amazon Linux 2017.03 v2.7.4 running Docker 17.03.2-ce"
   vpc_id                       = "vpc-a22222ee"
   zone_id                      = "ZXXXXXXXXXXX"
   public_subnets               = ["subnet-e63f82cb", "subnet-e66f44ab", "subnet-e88f42bd"]
   private_subnets              = ["subnet-e99d23eb", "subnet-e77e12bb", "subnet-e58a52bc"]
-  loadbalancer_type            = "application"
   loadbalancer_certificate_arn = "XXXXXXXXXXXXXXXXX"
   ssh_key_pair                 = "ssh-key-jenkins"
 
@@ -28,10 +26,6 @@ module "jenkins" {
   github_organization = "cloudposse"
   github_repo_name    = "jenkins"
   github_branch       = "master"
-
-  build_image        = "aws/codebuild/docker:1.12.1"
-  build_compute_type = "BUILD_GENERAL1_SMALL"
-  image_tag          = "latest"
 
   datapipeline_config = {
     instance_type = "t2.medium"
@@ -45,9 +39,6 @@ module "jenkins" {
     JENKINS_PASS          = "123456"
     JENKINS_NUM_EXECUTORS = 4
   }
-
-  delimiter  = "-"
-  attributes = []
 
   tags = {
     BusinessUnit = "ABC"
