2b39bdd91a84aa048c226a8746635c1f599ef7e6
Oscar <46930728+osulli@users.noreply.github.com>
2019-03-19 11:46:59 +0000

    Update EFS Backup to support AWS V2

--- main.tf
+++ main.tf
@@ -94,7 +94,7 @@ module "efs" {
 
 # EFS backup to S3
 module "efs_backup" {
-  source                             = "git::https://github.com/cloudposse/terraform-aws-efs-backup.git?ref=tags/0.8.0"
+  source                             = "git::https://github.com/osulli/terraform-aws-efs-backup.git?ref=patch-1"
   name                               = "${var.name}"
   stage                              = "${var.stage}"
   namespace                          = "${var.namespace}"
